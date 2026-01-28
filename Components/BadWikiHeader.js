2d2ab9101ea3cb8171265092747b4b397f3ae544
CandleLitScene <candlelitscene@gmail.com>
2026-01-27 21:07:57 -0500

    edits

---
+++ Components/BadWikiHeader.js
@@ -0,0 +1,27 @@
+class Header extends HTMLElement {
+    constructor() {
+      super();
+    }
+
+  connectedCallback() {
+    this.innerHTML = `
+      <header>
+        <nav>
+            <ul>
+                <li><a class="link" href="/index.html">Homepage</a></li>
+                <li><a class="link" href="../Brideshead.html">Brideshead Revisited</a></li>
+                <li><a class="link" href="../Pentiment.html">Pentiment</a></li>
+                <li><a class="link" href="/MyWork.html">My Work</a></li>
+                <li><a class="link" href="/Durer.html">Dürer</a></li>
+                <li><a class="link" href="/BillyBudd.html">Billy Budd</a></li>
+
+            </ul> 
+        </nav>
+        <h1>Candle-Lit Scene | Oddities: Questionable Wikipedia Photos</h1>
+        <p>This site is a continuous work in progress!</p>
+    </header>
+    `;
+  }
+}
+
+customElements.define('header-component', Header);
